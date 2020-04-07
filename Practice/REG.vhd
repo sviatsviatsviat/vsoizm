ba608bfaefbb8f036bb8ebaa982e66cd5d772582
Sviatoslau Kalosha <swetofdark@gmail.com>
2020-04-08 00:24:28 +0300

    Created Line Register

---
+++ Practice/REG.vhd
@@ -0,0 +1,61 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    21:53:20 04/07/2020 
+-- Design Name: 
+-- Module Name:    REG - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity REG is
+	 Generic (N : integer := 8);
+    Port ( CLK : in  STD_LOGIC;
+           RST : in  STD_LOGIC;
+           EN : in  STD_LOGIC;
+           REG_IN : in  STD_LOGIC_VECTOR(N-1 downto 0);
+           REG_OUT : out  STD_LOGIC_VECTOR(N-1 downto 0));
+end REG;
+
+architecture Behavioral of REG is
+
+signal reg: std_logic_vector(N-1 downto 0);
+
+begin
+
+processMain: process(RST, CLK, EN, REG_IN)
+begin
+	if (rst = '1') then
+		reg <= (others => '0');
+	elsif (EN = '1') then
+		if (rising_edge(CLK)) then
+			reg <=  REG_IN;
+		end if;
+	end if;
+end process;
+
+REG_OUT <= reg;
+
+end Behavioral;
+
